357fe5b04d557f270a5013190fe2482f5ad2b097
Antonio <55106060+NocturneLight@users.noreply.github.com>
2020-06-30 22:02:34 -0500

    Basic Hello World implemented.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Name: Antonio Luis Rios
+
+Note: Program designed with the SOLID principles of object-oriented programming in mind.
+
+*/
+
+using System;
+
+namespace Integer_To_English_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
